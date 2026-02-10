2f91f720765dfcf719510816af1034975274e6ee
阿根达斯 <2315916438@qq.com>
2026-02-11 00:11:20 +0800

    lab3

--- lab1/hkbu/chatbot.py
+++ lab1/hkbu/chatbot.py
@@ -3,21 +3,27 @@ This program requires the following modules:
 - python-telegram-bot==22.5
 - urllib3==2.6.2
 '''
+from collections import Counter
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import configparser
 import logging
-
+import requests
+from ChatGPT_HKBU import ChatGPT
+gpt = None
 def main():
+
     # Configure logging so you can see initialization and error messages
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    
+
     # Load the configuration data from file
     logging.info('INIT: Loading configuration...')
     config = configparser.ConfigParser()
     config.read('config.ini')
-
+    global gpt
+    gpt = ChatGPT(config)
     # Create an Application for your bot
     logging.info('INIT: Connecting the Telegram bot...')
     app = ApplicationBuilder().token(config['TELEGRAM']['ACCESS_TOKEN']).build()
@@ -31,11 +37,15 @@ def main():
     app.run_polling()
 
 async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await update.message.reply_text(response)
     logging.info("UPDATE: " + str(update))
+    loading_message = await update.message.reply_text('Thinking...')
 
-    # send the echo back to the client
-    text = update.message.text.upper()
-    await update.message.reply_text(text)
+    # send the user message to the ChatGPT client
+    response = gpt.submit(update.message.text)
+
+    # send the response to the Telegram box client
+    await loading_message.edit_text(response)
 
 if __name__ == '__main__':
     main()
